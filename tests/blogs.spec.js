f1afe3b886eabff5476a58c392afbbb7878f4fb6
Dmytro Lobanov <dspwww@gmail.com>
2025-01-18 01:26:08 +0000

    5.19 task DONE

--- tests/blogs.spec.js
+++ tests/blogs.spec.js
@@ -5,6 +5,13 @@ describe('Blog list app', () => {
   const username = 'test'
   const password = '12345678'
 
+  const loginWith = async (page, username, password, btnLabel) => {
+    await page.getByRole('textbox', {name: 'username'}).fill(username)
+    await page.getByRole('textbox', {name: 'password'}).fill(password)
+
+    await page.getByRole('button', { name: btnLabel }).click() 
+  }
+
   beforeEach(async ({page}) => {
     await page.goto('http://localhost:5173')
   })
@@ -20,33 +27,59 @@ describe('Blog list app', () => {
 
   describe('Login', () => {
     beforeEach(async ({ page, request }) => {
-      await request.post('http://localhost:3001/api/testing/reset')
-      await request.post('http://localhost:3001/api/users', {
+      await request.post('/api/testing/reset')
+      await request.post('/api/users', {
         data: {
           username: username,
           password: password
         }
       })
 
-      await page.goto('http://localhost:5173')
+      await page.goto('/')
     });
  
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByRole('textbox', {name: 'username'}).fill(username)
-      await page.getByRole('textbox', {name: 'password'}).fill(password)
-
-      await page.getByRole('button', { name: loginBtnLabel }).click() 
+      loginWith(page, username, password, loginBtnLabel);
 
       await expect(page.getByText(username)).toBeVisible();
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByRole('textbox', {name: 'username'}).fill(username)
-      await page.getByRole('textbox', {name: 'password'}).fill('1234567')
-  
-      await page.getByRole('button', { name: loginBtnLabel }).click() 
+      loginWith(page, username, '1234567', loginBtnLabel);
   
       await expect(page.getByText(/invalid username or password/)).toBeVisible();
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page, request }) => {
+      await request.post('/api/testing/reset')
+      await request.post('/api/users', {
+        data: {
+          username: username,
+          password: password
+        }
+      })
+
+      loginWith(page, username, password, loginBtnLabel);
+
+      await page.goto('/')
+    });
+  
+    test('a new blog can be created', async ({ page }) => {
+      const author = 'Wes Bos'
+      const title = 'Javascript and magic'
+      const url = 'wesbos.com/jsandmagic'
+
+      await page.getByRole('button', { name: /new blog/ }).click()
+
+      await page.locator('#blog-form-author').fill(author);
+      await page.locator('#blog-form-title').fill(title);
+      await page.locator('#blog-form-url').fill(url);
+
+      await page.getByRole('button', { name: 'Added new blog' }).click();
+
+      await expect(page.getByText(`${title} by ${author}`)).toBeVisible();
     })
   })
 })
